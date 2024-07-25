7aaaec81b02a4dd907fc7ef953a084868f6aa026
Yuliia <coconut3.dev@gmail.com>
2024-07-25 20:00:00 +0300

    Small adjustment to countdown

--- pages/goodnews.vue
+++ pages/goodnews.vue
@@ -61,14 +61,14 @@ export default {
   },
   data() {
     return {
-      targetDate: '2024-07-24T10:00:00',
+      targetDate: '2024-08-07T10:00:00',
       timerOutput: {
         days: 0,
         hours: 0,
         minutes: 0,
         seconds: 0
       },
-      ann_number: 'Announcement 5 of 7',
+      ann_number: 'Announcement 6 of 7',
       targetTimeZone: 'America/Los_Angeles',
       timerInterval: null
     };
@@ -103,8 +103,8 @@ export default {
       this.timerInterval = setInterval(countdown, 1000);
     },
     handleCountdownCompletion() {
-      this.ann_number = 'Announcement 6 of 7';
-      this.targetDate = '2024-08-07T10:00:00';
+      this.ann_number = 'Announcement 7 of 7';
+      this.targetDate = '2024-08-21T10:00:00';
       this.initializeTimer();
     }
   },
